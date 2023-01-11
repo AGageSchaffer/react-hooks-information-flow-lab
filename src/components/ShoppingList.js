a1446194a15a046e4ee8ace46c383949751e8ca6
AustinSchaffer <austinschaffer@gmail.com>
2023-01-11 13:26:50 -0600

    finish

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import Filter from "./Filter";
+import items from "../data/items";
 
-function ShoppingList({ items }) {
+function ShoppingList(isDarkMode) {
   const [selectedCategory, setSelectedCategory] = useState("All");
 
   function handleCategoryChange(event) {
@@ -17,12 +19,7 @@ function ShoppingList({ items }) {
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+        <Filter onCategoryChange={handleCategoryChange} />
       </div>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
